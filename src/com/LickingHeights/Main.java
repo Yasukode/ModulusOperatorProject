185a6ebfb52f745a3538b3bf1456d0157ece3a96
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-02 12:20:36 -0500

    even or odd test #1
    successful

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,12 @@
 package com.LickingHeights;
 
 public class Main {
-
+//todo: even or odd
+//todo: keep number contained
+//todo: get the last digit
     public static void main(String[] args) {
-	// OOOooooo
-    }
+	//Is the number even or odd?
+        int number = 4;
+	System.out.println("Number is even: "+(number % 2==0));
+    System.out.println("Number is odd: "+(number % 2 !=0));  }
 }
