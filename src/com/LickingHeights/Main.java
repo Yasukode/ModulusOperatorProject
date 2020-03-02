79376613dc93a03aa7cebd389138779296c2f5ab
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-02 12:30:44 -0500

    Test two
    guud

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,7 +6,7 @@ public class Main {
 //todo: get the last digit
     public static void main(String[] args) {
 	//Is the number even or odd?
-        int number = 4;
+        int number = 9;
 	System.out.println("Number is even: "+(number % 2==0));
     System.out.println("Number is odd: "+(number % 2 !=0));  }
 }
