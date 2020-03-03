7428dc5a78e760006b83b50ff056e5b646f6c505
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-03 12:17:19 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,12 +1,22 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        testOddness();
+    }
+//Is the number even or odd?
 //todo: even or odd
 //todo: keep number contained
 //todo: get the last digit
-    public static void main(String[] args) {
-	//Is the number even or odd?
-        int number = 9;
-	System.out.println("Number is even: "+(number % 2==0));
-    System.out.println("Number is odd: "+(number % 2 !=0));  }
+    public static void testOddness() {
+        Scanner userInput = new Scanner(System.in);
+        int positiveNumber;
+        System.out.println("Name any positive number.");
+        positiveNumber = userInput.nextInt();
+        System.out.println("The number is odd: "+(positiveNumber % 2 !=0));
+    }
 }
+
+
