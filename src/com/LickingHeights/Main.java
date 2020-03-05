be15efc7ac75c52a2a0b0f55f9dce89225994fe4
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-05 12:36:40 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,29 +6,45 @@ public class Main {
     public static void main(String[] args) {
         testOddness();
         keepContained();
+        lastDigit();
     }
 
 
-     //todo: even or odd
+    //todo: even or odd
+
     public static void testOddness() {//Is the number even or odd?
         Scanner userInput = new Scanner(System.in);
         int positiveNumber;
         System.out.println("Name any positive number.");
         positiveNumber = userInput.nextInt();
-        System.out.println("The number is odd: "+(positiveNumber%2!=0));
+        System.out.println("The number is odd: " + (positiveNumber % 2 != 0));
 
     }//todo: keep number contained
+
     public static void keepContained() {//We need to keep the number contained
-         Scanner userInput = new Scanner(System.in);
-         System.out.println("Give me another positive number.");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Give me another positive number.");
         int positiveNumberII = userInput.nextInt();
 
-        System.out.println("Number contained is: "+(positiveNumberII%8+5));
+        System.out.println("Number contained is: " + (positiveNumberII % 8 + 5));
 
+    }//todo: get the last digit
+
+    public static void lastDigit() {//We need to identify the last digit of any number
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Give me a number that is four spaces long.");
+        int fourDigitNumber = userInput.nextInt();
+        System.out.println("The last digit of the number is: "+(fourDigitNumber%10));
 
 
     }
-//todo: get the last digit
+
 }
+
+
+
+
+
+
 
 
