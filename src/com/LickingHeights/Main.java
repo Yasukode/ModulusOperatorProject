bf7f5c607bd42e0e503d826a918743de8b5a016f
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-05 12:24:40 -0500

    keepContainedMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,18 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         testOddness();
+        keepContained();
     }
-//Is the number even or odd?
-//todo: even or odd
-//todo: keep number contained
-//todo: get the last digit
-    public static void testOddness() {
+
+
+     //todo: even or odd
+    public static void testOddness() {//Is the number even or odd?
         Scanner userInput = new Scanner(System.in);
         int positiveNumber;
         System.out.println("Name any positive number.");
         positiveNumber = userInput.nextInt();
-        System.out.println("The number is odd: "+(positiveNumber % 2 !=0));
+        System.out.println("The number is odd: "+(positiveNumber%2!=0));
+
+    }//todo: keep number contained
+    public static void keepContained() {//We need to keep the number contained
+         Scanner userInput = new Scanner(System.in);
+         System.out.println("Give me another positive number.");
+        int positiveNumberII = userInput.nextInt();
+
+        System.out.println("Number contained is: "+(positiveNumberII%8+5));
+
+
+
     }
+//todo: get the last digit
 }
 
 
